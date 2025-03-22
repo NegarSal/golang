d0522105749e46472a4398aed394cd09fd61aaa7
Negar <negarsaljooghi@gmail.com>
2025-03-22 18:47:09 +0330

    declare variables: numbers(int, float)

--- main.go
+++ main.go
@@ -2,32 +2,27 @@ package main
 
 import "fmt"
 
-var someName = "hello"
-
 func main() {
 
-	//strings
+	//ints
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
 
-	//First way
-	var nameOne string = "mario"
+	fmt.Println(ageOne, ageTwo, ageThree)
 
-	//Second way
-	var nameTwo="luigi"
+	//bits & memory
+	var nameOne int8 = 25
+	var nameTwo int8 = -128
+	var nameThree uint8 = 25
 
-	//Third way
-	var nameThree string
+	fmt.Println(nameOne, nameTwo,  nameThree)
+	
+	//floats
+	var scoreOne float32 = -1.5
+	var scoreTwo float32 = 25.98
+	scoreThree := 1.5
 
-	fmt.Println(nameOne, nameTwo, nameThree)
-
-	//Update value
-
-	nameOne = "peach"
-	nameThree = "bowser"
-
-	fmt.Println(nameOne, nameTwo, nameThree)
-
-	//4th way
-	nameFour := "yoshi"
-	fmt.Println(nameFour)
+	fmt.Println(scoreOne, scoreTwo,  scoreThree)
 
 }
