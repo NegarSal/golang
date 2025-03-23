832536290cc444d0cbc2c3aa4ed21b46d3817a41
Negar <negarsaljooghi@gmail.com>
2025-03-23 13:03:37 +0330

    fmt package:Print & Println function

--- main.go
+++ main.go
@@ -3,26 +3,17 @@ package main
 import "fmt"
 
 func main() {
+	age := 35
+	name := "shaun"
 
-	//ints
-	var ageOne int = 20
-	var ageTwo = 30
-	ageThree := 40
+  //print
+  fmt.Print("Hello, ")
+  fmt.Print("world! \n")
+  fmt.Print("new line \n")
 
-	fmt.Println(ageOne, ageTwo, ageThree)
-
-	//bits & memory
-	var nameOne int8 = 25
-	var nameTwo int8 = -128
-	var nameThree uint8 = 25
-
-	fmt.Println(nameOne, nameTwo,  nameThree)
-	
-	//floats
-	var scoreOne float32 = -1.5
-	var scoreTwo float32 = 25.98
-	scoreThree := 1.5
-
-	fmt.Println(scoreOne, scoreTwo,  scoreThree)
+  //println	
+  fmt.Println("hello, ninjas!")
+  fmt.Println("goodbye, ninjas!")
+  fmt.Println("My age is:", age, "and my name is:" , name)
 
 }
