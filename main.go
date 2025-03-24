e3938ff2cd7d8ece776146277e7adc2947afaf53
Negar <negarsaljooghi@gmail.com>
2025-03-24 10:54:25 +0330

    fmt package:Printf

--- main.go
+++ main.go
@@ -16,4 +16,12 @@ func main() {
   fmt.Println("goodbye, ninjas!")
   fmt.Println("My age is:", age, "and my name is:" , name)
 
+  //Printf (formatted strings) %_ =format specifier
+  fmt.Printf("my age is %v and my name is %v \n", age, name)
+  fmt.Printf("my age is %q and my name is %q \n", age, name)
+  fmt.Printf("age is of type %T \n",age)
+  fmt.Printf("You scored %f points! \n", 225.55)
+  fmt.Printf("You scored %0.2f points! \n", 225.55)
+  fmt.Printf("You scored %0.1f points! \n", 225.55)
+  
 }
