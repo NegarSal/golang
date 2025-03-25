d5949bed1bb74f9c6c19cce29b6febd204ab500c
Negar <negarsaljooghi@gmail.com>
2025-03-25 15:58:02 +0330

    declare variables:array

--- main.go
+++ main.go
@@ -3,28 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	age := 35
-	name := "shaun"
 
-  //print
-  fmt.Print("Hello, ")
-  fmt.Print("world! \n")
-  fmt.Print("new line \n")
+  //Array
+  var ages1 [3]int = [3]int{20, 25, 30}
+  var ages2 = [3]int{20, 25, 30}
+  names := [4]string{"yoshi", "mario", "peach", "bowser"}
 
-  //println	
-  fmt.Println("hello, ninjas!")
-  fmt.Println("goodbye, ninjas!")
-  fmt.Println("My age is:", age, "and my name is:" , name)
+  fmt.Println(ages1, ages2)
+  fmt.Println(ages1, len(ages1))
+  fmt.Println(names, len(names))
 
-  //Printf (formatted strings) %_ =format specifier
-  fmt.Printf("my age is %v and my name is %v \n", age, name)
-  fmt.Printf("my age is %q and my name is %q \n", age, name)
-  fmt.Printf("age is of type %T \n",age)
-  fmt.Printf("You scored %f points! \n", 225.55)
-  fmt.Printf("You scored %0.2f points! \n", 225.55)
-  fmt.Printf("You scored %0.1f points! \n", 225.55)
-
-  //Sprintf (save formatted strings)
-  var str = fmt.Sprintf("my age is %v and my name is %v \n", age, name)
-  fmt.Println("The saved string is:", str)
 }
