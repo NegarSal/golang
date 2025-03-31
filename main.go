db4cb3e1af3b16b2eeeae033c549cb9e44fcb0dc
Negar <negarsaljooghi@gmail.com>
2025-03-31 19:40:14 +0330

    standard library packages: strings-Contains,ReplaceAll

--- main.go
+++ main.go
@@ -1,34 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
+  greeting := "hello there friends!"
 
-  //Array
-  var ages1 [3]int = [3]int{20, 25, 30}
-  var ages2 = [3]int{20, 25, 30}
-  names := [4]string{"yoshi", "mario", "peach", "bowser"}
-  names[1] = "luigi"
-
-  fmt.Println(ages1, ages2)
-  fmt.Println(ages1, len(ages1))
-  fmt.Println(names, len(names))
-
-  // slices (use arrays under the hood) 
-  var scores = []int{100, 50, 60}
-  scores[2] = 25
-  scores = append(scores, 85)
-
-  fmt.Println(scores, len(scores))
-
-  // slice ranges
-  rangeOne := names[1:3]
-  rangeTwo := names[2:]
-  rangeThree := names[:3]
-
-  fmt.Println(rangeOne, rangeTwo, rangeThree)
-
-  rangeOne = append(rangeOne, "Koopa")
-  fmt.Println(rangeOne)
+  fmt.Println(strings.Contains(greeting, "hello"))
+  fmt.Println(strings.Contains(greeting, "hello!"))
+  fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
+ 
+  // The original value is unchanged
+    fmt.Println("original string value =", greeting)
 
 }
