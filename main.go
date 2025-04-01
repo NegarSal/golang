320385450863a252b8ee4db36e1ae3133fdfa969
Negar <negarsaljooghi@gmail.com>
2025-04-01 20:36:35 +0330

    sort package: Ints, SearchInts

--- main.go
+++ main.go
@@ -2,21 +2,24 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"sort"
 )
 
 func main() {
-  greeting := "hello there friends!"
+  ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
 
-  //fmt.Println(strings.Contains(greeting, "hello"))
-  //fmt.Println(strings.Contains(greeting, "hello!"))
-  //fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
-  fmt.Println(strings.ToUpper(greeting))
-  fmt.Println(strings.Index(greeting,"ll"))
-  fmt.Println(strings.Index(greeting,"th"))
-  fmt.Println(strings.Split(greeting," "))
- 
-  // The original value is unchanged
-    fmt.Println("original string value =", greeting)
+  sort.Ints(ages)
+  fmt.Println(ages)
 
+  index := sort.SearchInts(ages, 30)
+  fmt.Println(index)
+
+  index2 := sort.SearchInts(ages, 90)
+  fmt.Println(index2)
+
+  names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
+  sort.Strings(names)
+  fmt.Println(names)
+  
+  fmt.Println(sort.SearchStrings(names, "bowser"))
 }
