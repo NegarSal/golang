68571be821aa0543c3239830b5efc2e9450114fe
Negar <negarsaljooghi@gmail.com>
2025-04-01 18:36:24 +0330

    standard library packages: strings-ToUpper, Index,Split

--- main.go
+++ main.go
@@ -8,9 +8,13 @@ import (
 func main() {
   greeting := "hello there friends!"
 
-  fmt.Println(strings.Contains(greeting, "hello"))
-  fmt.Println(strings.Contains(greeting, "hello!"))
-  fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
+  //fmt.Println(strings.Contains(greeting, "hello"))
+  //fmt.Println(strings.Contains(greeting, "hello!"))
+  //fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
+  fmt.Println(strings.ToUpper(greeting))
+  fmt.Println(strings.Index(greeting,"ll"))
+  fmt.Println(strings.Index(greeting,"th"))
+  fmt.Println(strings.Split(greeting," "))
  
   // The original value is unchanged
     fmt.Println("original string value =", greeting)
