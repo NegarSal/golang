678ac59c2458559f2ff7ff6f854c42d7cdf32448
Negar <negarsaljooghi@gmail.com>
2025-04-02 11:04:29 +0330

    for loops:example1

--- main.go
+++ main.go
@@ -2,24 +2,12 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
-  ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
-
-  sort.Ints(ages)
-  fmt.Println(ages)
-
-  index := sort.SearchInts(ages, 30)
-  fmt.Println(index)
-
-  index2 := sort.SearchInts(ages, 90)
-  fmt.Println(index2)
-
-  names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
-  sort.Strings(names)
-  fmt.Println(names)
-  
-  fmt.Println(sort.SearchStrings(names, "bowser"))
+	x := 0
+	for x < 5 {
+		fmt.Println("value of x is:", x)
+		x++
+	}
 }
