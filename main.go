b86e1d7c549f6e5068efbbe791cfb4433b7e68bf
Negar <negarsaljooghi@gmail.com>
2025-04-02 15:03:19 +0330

    for loops:example3

--- main.go
+++ main.go
@@ -5,13 +5,10 @@ import (
 )
 
 func main() {
-	// x := 0
-	// for x < 5 {
-	// 	fmt.Println("value of x is:", x)
-	// 	x++
-	// }
-  
-	for i := 0; i < 5; i++ {
-		fmt.Println("value of i is:", i)
+
+	names := []string{"mario", "luigi", "yoshi", "peach"}
+
+	for i := 0; i < len(names); i++ {
+		fmt.Println(names[i])
 	}
 }
