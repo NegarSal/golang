56c9bffdea95ad8cf0185510624a7ff332081d80
Negar <negarsaljooghi@gmail.com>
2025-04-02 11:42:17 +0330

    for loops:example2

--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import (
 )
 
 func main() {
-	x := 0
-	for x < 5 {
-		fmt.Println("value of x is:", x)
-		x++
+	// x := 0
+	// for x < 5 {
+	// 	fmt.Println("value of x is:", x)
+	// 	x++
+	// }
+  
+	for i := 0; i < 5; i++ {
+		fmt.Println("value of i is:", i)
 	}
 }
