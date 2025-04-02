b117394cb077e51a0226407fa17e094481b14f1e
Negar <negarsaljooghi@gmail.com>
2025-04-02 16:41:11 +0330

    for loops:example4

--- main.go
+++ main.go
@@ -8,7 +8,20 @@ func main() {
 
 	names := []string{"mario", "luigi", "yoshi", "peach"}
 
-	for i := 0; i < len(names); i++ {
-		fmt.Println(names[i])
+	// for i := 0; i < len(names); i++ {
+	// 	fmt.Println(names[i])
+	// }
+	for index, value := range names{
+		fmt.Printf("the value at index %v is %v \n", index, value)
 	}
+
+	for _, value := range names{
+		fmt.Printf("the value is %v \n", value)
+	}
+
+	for _, value := range names{
+		fmt.Printf("the value is %v \n", value)
+		value = "new string"
+	}
+	fmt.Println(names)
 }
