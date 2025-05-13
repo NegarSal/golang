fa442745edb2537027a85ee28c736fd0ab34937a
Negar <negarsaljooghi@gmail.com>
2025-05-13 11:59:55 +0330

    package scope1

--- main.go
+++ main.go
@@ -2,30 +2,13 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func getInitials(n string) (string, string) {
-	s := strings.ToUpper(n)
-	names := strings.Split(s, " ")
-
-	var initials []string
-	for _, v := range names {
-		initials = append(initials, v[:1])
-	}
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	}
-	return initials[0], "_"
-}
-
 func main() {
-	fn1, sn1 := getInitials("tifa lockhart")
-	fmt.Println(fn1, sn1)
 
-	fn2, sn2 := getInitials("cloud strife")
-	fmt.Println(fn2, sn2)
+	sayHello("mario")
 
-	fn3, sn3 := getInitials("barret")
-	fmt.Println(fn3, sn3)
+	for _, v := range points {
+		fmt.Println(v)
+	}
 }
