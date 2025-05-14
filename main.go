600c0506eda1b7c095a2c18709d2c34b2da89a9f
Negar <negarsaljooghi@gmail.com>
2025-05-14 19:04:32 +0330

    maps

--- main.go
+++ main.go
@@ -4,15 +4,13 @@ import (
 	"fmt"
 )
 
-var score = 99.5
-
 func main() {
-
-	sayHello("mario")
-
-	for _, v := range points {
-		fmt.Println(v)
+	menu := map[string]float64{
+		"soup":           4.99,
+		"pie":            7.99,
+		"salad":          6.99,
+		"toffee pudding": 3.55,
 	}
-
-	showScore()
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
 }
