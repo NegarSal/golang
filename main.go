0f4ee42e083ba15293a57d14e35cf392409adbd0
Negar <negarsaljooghi@gmail.com>
2025-05-15 21:41:45 +0330

    pass-by-value:groupB

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ func updateName(x string) string {
 	return x
 }
 
+func updateMenu(y map[string]float64) {
+	y["coffee"] = 2.99
+}
+
 func main() {
 	// group A types -> strings, ints, booleans, floats, arrays, structs
 	name := "tifa"
@@ -16,4 +20,13 @@ func main() {
 	name = updateName(name)
 
 	fmt.Println(name)
+
+	// group B types -> slices, maps, functions
+	menu := map[string]float64{
+		"pie":       5.95,
+		"ice cream": 3.99,
+	}
+
+	updateMenu(menu)
+	fmt.Println(menu)
 }
