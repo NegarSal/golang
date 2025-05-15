45b648843d953baba74667d601c1ab943fa5f05b
Negar <negarsaljooghi@gmail.com>
2025-05-15 18:07:03 +0330

    pass-by-value

--- main.go
+++ main.go
@@ -4,34 +4,15 @@ import (
 	"fmt"
 )
 
+func updateName(x string) {
+	x = "wedge"
+}
+
 func main() {
-	menu := map[string]float64{
-		"soup":           4.99,
-		"pie":            7.99,
-		"salad":          6.99,
-		"toffee pudding": 3.55,
-	}
-	fmt.Println(menu)
-	fmt.Println(menu["pie"])
+	// group A types -> strings, ints, booleans, floats, arrays, structs
+	name := "tifa"
 
-	// looping maps
-	for k, v := range menu {
-		fmt.Println(k, "-", v)
-	}
+	updateName(name)
 
-	// ints as key type
-	phonebook := map[int]string{
-		267584967: "mario",
-		984759373: "luigi",
-		845775485: "peach",
-	}
-	fmt.Println(phonebook)
-	fmt.Println(phonebook[267584967])
-
-	// update value
-	phonebook[984759373] = "bowser"
-	fmt.Println(phonebook)
-
-	phonebook[845775485] = "yoshi"
-	fmt.Println(phonebook)
+	fmt.Println(name)
 }
