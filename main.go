4de4a413ffcd5b5b7d4b48d8689510e6ed1aedfa
Negar <negarsaljooghi@gmail.com>
2025-05-19 21:47:21 +0330

    pointers3

--- main.go
+++ main.go
@@ -4,20 +4,16 @@ import (
 	"fmt"
 )
 
-func updateName(x string) {
-	x = "wedge"
+func updateName(x *string) {
+	*x = "wedge"
 }
 
 func main() {
 	name := "tifa"
 
-	updateName(name)
-
-	fmt.Println("memory address of name is:", &name)
-
 	m := &name
-	fmt.Println("memory address:", m)
-	fmt.Println("value at memory address:", *m)
 
+	fmt.Println(name)
+	updateName(m)
 	fmt.Println(name)
 }
