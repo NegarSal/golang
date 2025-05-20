f15c98d11b3697ee7dbdf77cd19a823f5bcdda13
Negar <negarsaljooghi@gmail.com>
2025-05-20 16:29:14 +0330

    create a new file bill.go

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
-func updateName(x *string) {
-	*x = "wedge"
-}
-
 func main() {
-	name := "tifa"
 
-	m := &name
-
-	fmt.Println(name)
-	updateName(m)
-	fmt.Println(name)
 }
